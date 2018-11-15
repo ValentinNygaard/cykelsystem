b3d4030c4655b185925812adf777c7fff54d1467
ValentinNygaard <rene930d@stud.kea.dk>
2018-11-15 10:23:53 +0100

    Initial commit

---
+++ src/main/java/com/example/cykelsystem/CykelsystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.cykelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CykelsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CykelsystemApplication.class, args);
+    }
+}
