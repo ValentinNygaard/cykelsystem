db8e632da0e8effe2316abf6dea10d4ebbb84757
erik <erikpilverdier1@gmail.com>
2018-12-03 14:57:15 +0100

    customer_id i bicycle

--- src/main/java/com/example/cykelsystem/Model/Bicycle.java
+++ src/main/java/com/example/cykelsystem/Model/Bicycle.java
@@ -9,13 +9,23 @@ public class Bicycle {
     @Id
     private int bicycle_id;
     private String description;
+    private int customer_id;
 
-    public Bicycle(int bicycle_id, String description) {
+    public Bicycle(int bicycle_id, String description, int customer_id) {
         this.bicycle_id = bicycle_id;
         this.description = description;
+        this.customer_id = customer_id;
     }
 
     public Bicycle(){
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
     }
 
     public int getBicycle_id() {
