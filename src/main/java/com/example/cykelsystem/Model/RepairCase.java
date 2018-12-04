cce002908e39bb9a423a4076c43146c432634e46
erik <erikpilverdier1@gmail.com>
2018-12-04 12:51:07 +0100

    repaircase

--- src/main/java/com/example/cykelsystem/Model/RepairCase.java
+++ src/main/java/com/example/cykelsystem/Model/RepairCase.java
@@ -11,13 +11,20 @@ public class RepairCase {
     private int repair_case_id;
     private Date start_date;
     private Date end_date;
-    private int status;
+    private int status_id;
+    private int bicycle_id;
+    private int employee_id;
+    private int repair_number;
 
-    public RepairCase(int repair_case_id, Date start_date, Date end_date, int status) {
+    public RepairCase(int repair_case_id, Date start_date, Date end_date,
+                      int status_id, int bicycle_id, int employee_id, int repair_number){
         this.repair_case_id = repair_case_id;
         this.start_date = start_date;
         this.end_date = end_date;
-        this.status = status;
+        this.status_id = status_id;
+        this.bicycle_id = bicycle_id;
+        this.employee_id = employee_id;
+        this.repair_number = repair_number;
     }
 
     public RepairCase(){}
@@ -46,11 +53,35 @@ public class RepairCase {
         this.end_date = end_date;
     }
 
-    public int getStatus() {
-        return status;
+    public int getStatus_id() {
+        return status_id;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    public void setStatus_id(int status_id) {
+        this.status_id = status_id;
+    }
+
+    public int getBicycle_id() {
+        return bicycle_id;
+    }
+
+    public void setBicycle_id(int bicycle_id) {
+        this.bicycle_id = bicycle_id;
+    }
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public int getRepair_number() {
+        return repair_number;
+    }
+
+    public void setRepair_number(int repair_number) {
+        this.repair_number = repair_number;
     }
 }
