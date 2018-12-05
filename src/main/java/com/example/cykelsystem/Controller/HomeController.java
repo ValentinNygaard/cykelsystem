694d54de8a278c15e59906ed499d24330336af2f
EmilieBentsen <emilie.bentsen@gmail.com>
2018-12-05 22:10:28 +0100

    bootstraptest

--- src/main/java/com/example/cykelsystem/Controller/HomeController.java
+++ src/main/java/com/example/cykelsystem/Controller/HomeController.java
@@ -1,20 +1,9 @@
 package com.example.cykelsystem.Controller;
 
 
-import com.example.cykelsystem.Model.StdRepairLineItem;
-import com.example.cykelsystem.Service.IService;
-import com.example.cykelsystem.Service.StatusServiceImpl;
-import com.example.cykelsystem.Service.StdRepairLineItemServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.example.cykelsystem.Model.Status;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
@@ -25,7 +14,11 @@ public class HomeController {
        return "home/index";
     }
 
-
+   /*
+    @GetMapping("/")
+    public String home(Model model){
+        return "bootstrapemilie/bootstraptest";
+    }*/
 
 
 
