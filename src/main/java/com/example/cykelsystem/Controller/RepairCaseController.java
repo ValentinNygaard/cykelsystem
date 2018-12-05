0fc6c12ef88dfec4df587529e324c26558da7bda
erik <erikpilverdier1@gmail.com>
2018-12-05 11:13:56 +0100

    repaircase

---
+++ src/main/java/com/example/cykelsystem/Controller/RepairCaseController.java
@@ -0,0 +1,63 @@
+package com.example.cykelsystem.Controller;
+
+import com.example.cykelsystem.Model.RepairCase;
+import com.example.cykelsystem.Service.RepairCaseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class RepairCaseController {
+
+    @Autowired
+    RepairCaseServiceImpl rcsi;
+
+    @GetMapping("/repaircase")
+    public String repairCase(Model model){
+        List<RepairCase> repairCases = rcsi.findAll();
+        model.addAttribute("repairCase", repairCases);
+        return "repaircase/repaircase";
+    }
+
+    @GetMapping("/detailsRepairCase/{id}")
+    public String detailsrepaircase(@PathVariable("id")int id, Model model){
+        model.addAttribute("repairCase", rcsi.findById(id));
+        return "repaircase/detailsrepaircase";
+    }
+
+    @GetMapping("/deleteRepairCase/{id}")
+    public String deleteRepairCase(@PathVariable("id")int id)
+    {
+        boolean deleted = rcsi.delete(id);
+        if(deleted) {
+            return "redirect:/";
+        }
+        else{
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/updateRepairCase/{id}")
+    public String updateRepairCase(@PathVariable("id")int id, Model model) {
+        model.addAttribute("RepairCase",rcsi.findById(id));
+        return "repaircase/updaterepaircase";
+    }
+
+    @PostMapping("/updateRepairCase")
+    public String updateRepairCase(@ModelAttribute RepairCase repairCase){
+        rcsi.update(repairCase);
+        return "redirect:/";
+    }
+
+    @PostMapping("/createRepairCase")
+    public String createRepairCase(@ModelAttribute RepairCase repairCase){
+        rcsi.create(repairCase);
+        return "redirect:/";
+    }
+}
--- src/main/java/com/example/cykelsystem/Repository/RepairCaseRepoImpl.java
+++ src/main/java/com/example/cykelsystem/Repository/RepairCaseRepoImpl.java
@@ -2,7 +2,9 @@ package com.example.cykelsystem.Repository;
 
 import com.example.cykelsystem.Model.RepairCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,26 +17,45 @@ public class RepairCaseRepoImpl implements IRepo<RepairCase> {
 
     @Override
     public List<RepairCase> findAll() {
-        return null;
+        String sql = "SELECT * FROM repair_case";
+        RowMapper<RepairCase> rowMapper = new BeanPropertyRowMapper<>(RepairCase.class);
+        List<RepairCase> repairCases = template.query(sql, rowMapper);
+        return repairCases;
     }
 
     @Override
     public RepairCase findById(int id) {
-        return null;
+        String sql = "SELECT * FROM repair_case WHERE repair_case_id=?";
+        RowMapper<RepairCase> rowMapper = new BeanPropertyRowMapper<>(RepairCase.class);
+        RepairCase repairCase = template.queryForObject(sql, rowMapper, id);
+        return repairCase;
     }
 
     @Override
     public RepairCase create(RepairCase repairCase) {
-        return null;
+        String sql = "INSERT INTO repair_case (repair_case_id, start_date, end_date, status_id, bicycle_id, employee_id, repair_number) VALUES(?,?,?,?,?,?,?)";
+        template.update(sql, repairCase.getRepair_case_id(), repairCase.getStart_date(),repairCase.getEnd_date(), repairCase.getStatus_id(),
+                repairCase.getBicycle_id(), repairCase.getEmployee_id(), repairCase.getRepair_number());
+        return repairCase;
     }
 
     @Override
     public RepairCase update(RepairCase repairCase) {
-        return null;
+        String sql = "UPDATE repair_case SET start_date=?, end_date=?, status_id=?, bicycle_id=?,employee_id=?, repair_number=? WHERE repair_case_id=?";
+        template.update(sql, repairCase.getStart_date(), repairCase.getEnd_date(), repairCase.getStatus_id(),
+                repairCase.getBicycle_id(), repairCase.getEmployee_id(), repairCase.getRepair_number(), repairCase.getRepair_case_id());
+        RepairCase newRepairCase = findById(repairCase.getRepair_case_id());
+        return newRepairCase;
     }
 
     @Override
-    public boolean delete(int id) {
-        return false;
-    }
+    public boolean delete(int id){
+    String sql = "DELETE FROM repair_case WHERE repair_case_id=?";
+        return template.update(sql, id) >= 0;    }
 }
+
+
+
+
+
+
