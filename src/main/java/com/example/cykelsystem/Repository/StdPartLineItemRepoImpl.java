87dedbdae575014a65ce148efb8bd5df9bd63d1a
ValentinNygaard <rene930d@stud.kea.dk>
2018-12-06 18:07:28 +0100

    renaming

--- src/main/java/com/example/cykelsystem/Repository/StdPartLineItemRepoImpl.java
+++ src/main/java/com/example/cykelsystem/Repository/StdPartLineItemRepoImpl.java
@@ -18,35 +18,35 @@ public class StdPartLineItemRepoImpl implements IRepo<StdPartItemLine> {
 
     @Override
     public List<StdPartItemLine> findAll(){
-        String sql = "SELECT * FROM std_bicyclepart_line_item";
+        String sql = "SELECT * FROM std_part_line_item";
         RowMapper<StdPartItemLine> rowMapper = new BeanPropertyRowMapper<>(StdPartItemLine.class);
         return template.query(sql, rowMapper);
     }
 
     @Override
     public StdPartItemLine findById(int id){
-        String sql = "SELECT * FROM std_bicycle_line_item WHERE std_bicycle_line_item_id=?";
+        String sql = "SELECT * FROM std_part_line_item WHERE std_part_line_item_id=?";
         RowMapper<StdPartItemLine> rowMapper = new BeanPropertyRowMapper<>(StdPartItemLine.class);
         return template.queryForObject(sql, rowMapper, id);
     }
 
     @Override
     public StdPartItemLine create(StdPartItemLine sbli) {
-        String sql = "INSERT INTO std_bicycle_line_item (std_bicycle_line_item_id, description, price) VALUES(?,?,?)";
+        String sql = "INSERT INTO std_part_line_item (std_part_line_item_id, description, price) VALUES(?,?,?)";
         template.update(sql, sbli.getStd_part_line_item_id(), sbli.getDescription(), sbli.getPrice(), sbli.getPrice());
         return sbli;
     }
 
     @Override
     public StdPartItemLine update(StdPartItemLine sbli) {
-        String sql = "UPDATE std_bicyclepart_line_item SET description=?, price=? WHERE std_bicycle_line_item_id=?";
+        String sql = "UPDATE std_part_line_item SET description=?, price=? WHERE std_part_line_item_id=?";
         template.update(sql, sbli.getDescription(), sbli.getPrice(), sbli.getStd_part_line_item_id());
         return sbli;
     }
 
     @Override
     public boolean delete(int id) {
-        String sql = "DELETE FROM std_bicyclepart_line_item WHERE std_bicycle_line_item_id=?";
+        String sql = "DELETE FROM std_part_line_item WHERE std_part_line_item_id=?";
         return template.update(sql, id) >= 0;
     }
 }
