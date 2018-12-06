a039fead6f450202919d98ccdd7be06ce7de7897
EmilieBentsen <emilie.bentsen@gmail.com>
2018-12-06 10:23:45 +0100

    bootstraptest

--- src/main/java/com/example/cykelsystem/Controller/HomeController.java
+++ src/main/java/com/example/cykelsystem/Controller/HomeController.java
@@ -9,16 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
 
-   @GetMapping("/")
+   /*@GetMapping("/")
     public String home(Model model){
        return "home/index";
-    }
+    }*/
 
-   /*
+
     @GetMapping("/")
     public String home(Model model){
         return "bootstrapemilie/bootstraptest";
-    }*/
+    }
 
 
 
