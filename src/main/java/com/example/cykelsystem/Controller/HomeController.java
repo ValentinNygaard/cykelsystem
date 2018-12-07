a6a71cd8af21487ee9542dcdd01c5b66cb15ca3d
EmilieBentsen <emilie.bentsen@gmail.com>
2018-12-07 11:30:43 +0100

    bootstraptest

--- src/main/java/com/example/cykelsystem/Controller/HomeController.java
+++ src/main/java/com/example/cykelsystem/Controller/HomeController.java
@@ -1,22 +1,34 @@
 package com.example.cykelsystem.Controller;
 
 
+import com.example.cykelsystem.Model.RepBarLine;
+import com.example.cykelsystem.Service.RepBarLineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    RepBarLineService rbls;
 
-   @GetMapping("/")
+   /*@GetMapping("/")
     public String home(Model model){
        return "home/index";
-    }
-
-
-    /*@GetMapping("/")
-    public String home(Model model){
-        return "bootstrapemilie/bootstraptest";
     }*/
+
+
+    @GetMapping("/")
+    public String home(Model model){
+        List<RepBarLine> repbarlines = rbls.findAll();
+        model.addAttribute("repBarLine", repbarlines);
+        for(RepBarLine i : repbarlines){
+            System.out.println(i.getPhone_number());
+        }
+        return "bootstrapemilie/bootstraptest";
+    }
 }
