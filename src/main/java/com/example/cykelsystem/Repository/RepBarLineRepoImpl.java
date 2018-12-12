029838aa7bb3fa88fa87145a3cfed730710fd978
erik <erikpilverdier1@gmail.com>
2018-12-12 11:50:28 +0100

    id test

--- src/main/java/com/example/cykelsystem/Repository/RepBarLineRepoImpl.java
+++ src/main/java/com/example/cykelsystem/Repository/RepBarLineRepoImpl.java
@@ -42,13 +42,13 @@ public class RepBarLineRepoImpl {
     }
 
     public List<RepBarLine> findAllRepbarsDate(java.sql.Date date) {
-        String sql = "select repair_case_id, phone_number, repair_number, t1.time, t1.price, status_title, end_date\n" +
-                "from customer natural join bicycle natural join repair_case natural join (select repair_case_id, status_title, sum(time) as time, sum(price) as price\n" +
-                "from status natural join repair_case natural join repair_line_item\n" +
-                "group by repair_case_id) t1\n" +
-                "where status_id < 3 and end_date >= ?" +
-                "group by repair_case_id\n" +
-                "order by end_date, status_id;";
+        String sql = "SELECT repair_case_id, phone_number, repair_number, t1.time, t1.price, status_title, end_date\n" +
+                "FROM customer NATURAL JOIN bicycle NATURAL JOIN repair_case NATURAL JOIN (SELECT repair_case_id, status_title, sum(time) AS time, sum(repair_line_item.price + part_line_item.price) AS price\n" +
+                "FROM status NATURAL JOIN repair_case NATURAL JOIN repair_line_item LEFT JOIN part_line_item using(repair_case_id)\n" +
+                "GROUP BY repair_case_id) t1\n" +
+                "WHERE status_id < 3 AND end_date >= ?\n" +
+                "GROUP BY repair_case_id\n" +
+                "ORDER BY end_date, status_id;";
         RowMapper<RepBarLine> rowMapper = new BeanPropertyRowMapper<>(RepBarLine.class);
         List<RepBarLine> repBarLines = template.query(sql, rowMapper, date);
         return repBarLines;
