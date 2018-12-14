dbadaadb461ddbce0da8a6897210817bd8070642
erik <erikpilverdier1@gmail.com>
2018-12-14 11:20:56 +0100

    mer sql

--- src/main/java/com/example/cykelsystem/Repository/RepBarLineRepoImpl.java
+++ src/main/java/com/example/cykelsystem/Repository/RepBarLineRepoImpl.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -100,37 +99,4 @@ public class RepBarLineRepoImpl {
         List<RepBarLine> repBarLines = template.query(sql, rowMapper, fromStatus, toStatus, fromDate, toDate);
         return repBarLines;
     }
-
-
-
-
-    /*sql statement med null værdier
-        SELECT repair_case_id, phone_number, repair_number, t1.time, t1.price, status_title, end_date
-FROM customer NATURAL JOIN bicycle
-	NATURAL JOIN repair_case
-	LEFT JOIN
-    (SELECT repair_case_id, status_title, sum(time) as time, sum(price) as price
-	FROM status NATURAL JOIN repair_case
-		LEFT JOIN repair_line_item using(repair_case_id)
-	GROUP BY repair_case_id) t1 using(repair_case_id)
-GROUP BY repair_case_id
-ORDER BY end_date, status_id;*/
-
-        /*sql statement 2 med null værdier
-        SELECT repair_case_id, sum(price) as price
-FROM part_line_item
-GROUP BY repair_case_id
-ORDER BY repair_case_id;
-         */
-
-        /*sql statement hvor der hentes pris til en enkelt repair case
-	SELECT repair_case_id, sum(repair_line_item.price) as rep_price, sum(part_line_item.price) as part_price
-	FROM repair_line_item LEFT JOIN part_line_item using(repair_case_id)
-	where repair_case_id = ?
-UNION
-	SELECT repair_case_id, sum(repair_line_item.price) as rep_price, sum(part_line_item.price) as part_price
-	FROM repair_line_item RIGHT JOIN part_line_item using(repair_case_id)
-	where repair_case_id = ?;
-        */
-
 }
