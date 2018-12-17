73a373711533aac9f8b7943879810767b845fd21
ValentinNygaard <rene930d@stud.kea.dk>
2018-12-17 22:29:49 +0100

    Simpel oversigtsside

--- src/main/java/com/example/cykelsystem/Controller/OverviewController.java
+++ src/main/java/com/example/cykelsystem/Controller/OverviewController.java
@@ -23,9 +23,18 @@ public class OverviewController {
      after that, we are returning the HTML site. */
     @GetMapping("/overview")
     public String overview(Model model){
-        //List<RepBarLine> repBarLines = rbls.findAll();
-        List<RepBarLine> repBarLines = rbls.findRepbarsWithStatusFromDate(1,6, "2017-12-01");
-        model.addAttribute("repBarLine", repBarLines);
+        List<RepBarLine> repBarLinesO = rbls.findRepbarsWithStatusFromDate(2,2, "2017-12-01");
+        model.addAttribute("repBarLineO", repBarLinesO);
+        List<RepBarLine> repBarLinesR = rbls.findRepbarsWithStatusFromDate(3,3, "2017-12-01");
+        model.addAttribute("repBarLineR", repBarLinesR);
+        List<RepBarLine> repBarLinesA = rbls.findRepbarsWithStatusFromDate(4,4, "2017-12-01");
+        model.addAttribute("repBarLineA", repBarLinesA);
+        List<RepBarLine> repBarLinesU = rbls.findRepbarsWithStatusFromDate(1,1, "2017-12-01");
+        model.addAttribute("repBarLineU", repBarLinesU);
+        List<RepBarLine> repBarLinesP = rbls.findRepbarsWithStatusFromDate(6,6, "2017-12-01");
+        model.addAttribute("repBarLineP", repBarLinesP);
+        List<RepBarLine> repBarLinesK = rbls.findRepbarsWithStatusFromDate(5,5, "2017-12-01");
+        model.addAttribute("repBarLineK", repBarLinesK);
         return "overview/overview";
     }
 }
