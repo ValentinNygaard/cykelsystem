b819abe2bed3494fa97389d9b3cf503ab7dab256
ValentinNygaard <rene930d@stud.kea.dk>
2018-12-18 09:17:52 +0100

    gammel overviewside

--- src/main/java/com/example/cykelsystem/Controller/OverviewController.java
+++ src/main/java/com/example/cykelsystem/Controller/OverviewController.java
@@ -15,16 +15,17 @@ public class OverviewController {
 
     @Autowired
     RepBarLineService rbls;                      // Her opretter vi et objekt af RepBarLineService
-    ServiceService service;                     //Overflødig?
 
+    ServiceService service = new ServiceService();                     //Overflødig?
+
+    private String currentDate = service.getDateForToday();
     /* This method is refering to the HTML site "overview", in the method we are creating a list of RepBarLIne,
      we are filling the list with a method in RepBarLineService, the method findRepBarsWithStatusFromDate.
      then we are adding the elements to a Model object, with the method addAttribute.
      after that, we are returning the HTML site. */
     @GetMapping("/overview")
     public String overview(Model model){
-        //List<RepBarLine> repBarLines = rbls.findAll();
-        List<RepBarLine> repBarLines = rbls.findRepbarsWithStatusFromDate(1,6, service.getDateForToday());
+        List<RepBarLine> repBarLines = rbls.findRepbarsWithStatusFromDate(2,3, currentDate);
         model.addAttribute("repBarLine", repBarLines);
         return "overview/overview";
     }
