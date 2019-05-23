5442ea89944ef2b0ee743b86d236e6350d82f4bc
erik <erikpilverdier1@gmail.com>
2019-05-23 11:26:25 +0200

    5 repbar

--- src/main/java/com/example/cykelsystem/Controller/OverviewController.java
+++ src/main/java/com/example/cykelsystem/Controller/OverviewController.java
@@ -38,22 +38,41 @@ public class OverviewController {
      after that, we are returning the HTML site. */
     @GetMapping("/overview")
     public String overview(Model model) {
-        String curentDate = service.getDateForToday();
-        String tomorrow = service.getDateForTomorrow();
+        String day0 = service.getDateForToday();
+        String day1 = service.getDateForTomorrow();
+        String day2 = service.getDateForLaterThanToday(2);
+        String day3 = service.getDateForLaterThanToday(3);
+        String day4 = service.getDateForLaterThanToday(4);
 
-        /*List<Employee> employeelist = esi.findAllActive();
-        model.addAttribute("employee", employeelist);*/
         List<Status> statusList = ss.findAll();
         model.addAttribute("status", statusList);
-        List<RepBarLine> repBarLinesO = rbls.findRepbarsWithStatusBetweenDates(2,2, curentDate, curentDate);
-        model.addAttribute("repBarLineO", repBarLinesO);
-        List<RepBarLine> repBarLinesR = rbls.findRepbarsWithStatusBetweenDates(3,3, curentDate, curentDate);
-        model.addAttribute("repBarLineR", repBarLinesR);
-        List<RepBarLine> repBarLinesOT = rbls.findRepbarsWithStatusBetweenDates(2,2, tomorrow, tomorrow);
-        model.addAttribute("repBarLineOT", repBarLinesOT);
-        List<RepBarLine> repBarLinesRT = rbls.findRepbarsWithStatusBetweenDates(3,3, tomorrow, tomorrow);
-        model.addAttribute("repBarLineRT", repBarLinesRT);
-        List<RepBarLine> repBarLines = rbls.findRepbarsWithStatusFromDate(2, 3, service.getDateForLaterThanToday(2));
+
+        List<RepBarLine> repBarLinesD0 = rbls.findRepbarsWithStatusBetweenDates(1,3, day0, day0);
+        model.addAttribute("repBarLineD0", repBarLinesD0);
+        List<RepBarLine> repBarLinesFD0 = rbls.findRepbarsWithStatusBetweenDates(4,4, day0, day0);
+        model.addAttribute("repBarLineFD0", repBarLinesFD0);
+
+        List<RepBarLine> repBarLinesD1 = rbls.findRepbarsWithStatusBetweenDates(1,3, day1, day1);
+        model.addAttribute("repBarLineD1", repBarLinesD1);
+        List<RepBarLine> repBarLinesFD1 = rbls.findRepbarsWithStatusBetweenDates(4,4, day1, day1);
+        model.addAttribute("repBarLineFD1", repBarLinesFD1);
+
+        List<RepBarLine> repBarLinesD2 = rbls.findRepbarsWithStatusBetweenDates(1,3, day2, day2);
+        model.addAttribute("repBarLineD2", repBarLinesD2);
+        List<RepBarLine> repBarLinesFD2 = rbls.findRepbarsWithStatusBetweenDates(4,4, day2, day2);
+        model.addAttribute("repBarLineFD2", repBarLinesFD2);
+
+        List<RepBarLine> repBarLinesD3 = rbls.findRepbarsWithStatusBetweenDates(1,3, day3, day3);
+        model.addAttribute("repBarLineD3", repBarLinesD3);
+        List<RepBarLine> repBarLinesFD3 = rbls.findRepbarsWithStatusBetweenDates(4,4, day3, day3);
+        model.addAttribute("repBarLineFD3", repBarLinesFD3);
+
+        List<RepBarLine> repBarLinesD4 = rbls.findRepbarsWithStatusBetweenDates(1,3, day4, day4);
+        model.addAttribute("repBarLineD4", repBarLinesD4);
+        List<RepBarLine> repBarLinesFD4 = rbls.findRepbarsWithStatusBetweenDates(4,4, day4, day4);
+        model.addAttribute("repBarLineFD4", repBarLinesFD4);
+
+        List<RepBarLine> repBarLines = rbls.findRepbarsWithStatusFromDate(1, 4, service.getDateForLaterThanToday(2));
         model.addAttribute("repBarLine", repBarLines);
         return "overview/overview";
     }
